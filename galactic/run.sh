e25c0162d76d19100ac3ae42abda33b34880abe4
macmacal <mac.aleksandrowicz@gmail.com>
2023-01-19 00:46:04 +0100

    Instalation of python scripts

--- galactic/run.sh
+++ galactic/run.sh
@@ -10,14 +10,19 @@ source /root/ros1_ws/install_isolated/setup.bash
 
 # roscore must be run before sourcing ROS2
 roscore -p 11311 & >/dev/null 2>&1
+
+# run AMBF with Surgical Robotics Challenge
 /root/ros1_ws/src/ambf/bin/lin-x86_64/ambf_simulator --launch_file /root/surgical_robotics_challenge/launch.yaml -l 0,1,3,4,14,15 -p 120 -t 1 --override_max_comm_freq 120 &
+sleep 5 # ensure that everything is running
+python3 /root/surgical_robotics_challenge/scripts/surgical_robotics_challenge/launch_crtk_interface.py &
+
 
 # unsetting ROS_DISTRO to silence ROS_DISTRO override warning
 unset ROS_DISTRO
 # setup ros2 environment
 source /opt/ros/$ROS2_DISTRO/setup.bash
 
-echo "> ROS1 & ROS2 sourced, ros1 & ambf has been launched"
+echo "> ROS1 & ROS2 sourced, ros1 & ambf has been launched" 
 
 # run dynamic ros1-ros2 bridge in background
 source /root/ros2_ws/install/local_setup.bash
